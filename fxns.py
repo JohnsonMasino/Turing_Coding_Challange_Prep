09e8bc2455d4637fe6b7d636ae19162f8eccf9ad
Johnson Masino <johnsonmasino@gmail.com>
2024-04-19 05:58:25 +0100

    Added a code

---
+++ fxns.py
@@ -0,0 +1,13 @@
+"""
+This program lists the function in python
+"""
+import datetime
+
+
+def fxn():
+    print(dir(__builtins__))
+    print("\n", dir(datetime))
+
+if __name__ == "__main__":
+    fxn()
+print("\nCode developed by Masino")
